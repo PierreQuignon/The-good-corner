9bf00709d66bb915313dec20b4092e04766d1539
Pierre Quignon <p.quignon.pro@gmail.com>
2023-09-29 09:38:59 +0200

    bug fixed the new ad page

--- frontend/src/pages/ads/new.tsx
+++ frontend/src/pages/ads/new.tsx
@@ -1,122 +1,3 @@
-// import { CategoryProps } from "@/components/Category";
-// import axios from "axios";
-// import { FormEvent, useEffect, useState } from "react";
-// import { useRouter } from "next/router";
-
-// type AdFormData = {
-//   title: string;
-//   description: string;
-//   price: number;
-//   category: { id: number };
-// };
-
-// const NewAd = () => {
-//   const [categories, setCategories] = useState<CategoryProps[]>([]);
-//   const [error, setError] = useState<"title" | "price">();
-//   const [hasBeenSent, setHasBeenSent] = useState<true | false>();
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     axios
-//       .get("http://localhost:5000/categories")
-//       .then((result) => {
-//         setCategories(result.data);
-//       })
-//       .catch((err) => {
-//         console.error(err);
-//       });
-//   }, []);
-
-//   async function onSubmit(event: FormEvent<HTMLFormElement>) {
-//     event.preventDefault();
-//     setError(undefined);
-//     const form = event.target as HTMLFormElement;
-//     const formData = new FormData(form);
-//     const data = Object.fromEntries(
-//       formData.entries()
-//     ) as unknown as AdFormData;
-
-//     if ("categoryId" in data) {
-//       data.category = { id: Number(data.categoryId) };
-//       delete data.categoryId;
-//     }
-
-//     data.price = Number(data.price);
-
-//     if (data.title.trim().length < 3) {
-//       setError("title");
-//     } else if (data.price < 0) {
-//       setError("price");
-//     } else {
-//       const result = await axios.post("http://localhost:5000/ads", data);
-//       if ("id" in result.data) {
-//         form.reset();
-//         setHasBeenSent(true);
-//         const adSentId = result.data.id;
-//         router.push(`/ads/${adSentId}`);
-//       }
-//     }
-//   }
-
-//   return (
-//     <main className="main-content">
-//       <form onSubmit={onSubmit}>
-//         <label>
-//           {error === "price" && <p>Le prix doit être positif</p>}
-//           {error === "title" && (
-//             <p>Le titre est requis et doit faire plus de 3 caractères</p>
-//           )}
-//           {hasBeenSent && <p>Offre ajoutée !</p>}
-//           Titre de l&apos;annonce :
-//           <br />
-//           <input className="text-field" type="text" name="title" />
-//           <br />
-//           <br />
-//           Description :
-//           <br />
-//           <input className="text-field" type="text" name="description" />
-//           <br />
-//           <br />
-//           Propriétaire :
-//           <br />
-//           <input className="text-field" type="text" name="owner" />
-//           <br />
-//           <br />
-//           Prix :
-//           <br />
-//           <input className="text-field" type="number" name="price" />
-//           <br />
-//           <br />
-//           Localisation :
-//           <br />
-//           <input className="text-field" type="text" name="location" />
-//           <br />
-//           <br />
-//           Photo :
-//           <br />
-//           <input className="text-field" type="text" name="picture" />
-//           <br />
-//           <br />
-//           Catégorie :
-//           <br />
-//           <select name="category" className="text-field">
-//             {categories.map((categ) => (
-//               <option value={categ.id} key={categ.id}>
-//                 {categ.type}
-//               </option>
-//             ))}
-//           </select>
-//         </label>
-//         <br />
-//         <br />
-//         <button className="button">Soumettre</button>
-//       </form>
-//     </main>
-//   );
-// };
-
-// export default NewAd;
-
 import { CategoryType } from "@/components/Category";
 import { Layout } from "@/components/Layout";
 import axios from "axios";
@@ -179,7 +60,7 @@ export default function NewAd() {
     } else if (data.price < 0) {
       setError("price");
     } else {
-      const result = await axios.post("http://localhost:5000/categories", data);
+      const result = await axios.post("http://localhost:5000/ads", data);
       if ("id" in result.data) {
         setTitle("");
         setDescription("");
@@ -190,8 +71,6 @@ export default function NewAd() {
         setLocation("");
 
         setHasBeenSent(true);
-        // redirect to /ads/result.data.id
-        // https://nextjs.org/docs/pages/api-reference/functions/use-router
         setTimeout(() => {
           router.push(`/ads/${result.data.id}`);
         }, 5000);
@@ -213,6 +92,7 @@ export default function NewAd() {
             )}
             <form onSubmit={onSubmit}>
               <input
+                className="text-field"
                 type="text"
                 name="title"
                 placeholder="Titre de l'annonce"
@@ -222,6 +102,7 @@ export default function NewAd() {
               <br />
               <br />
               <input
+                className="text-field"
                 type="text"
                 name="description"
                 placeholder="Description de l'annonce"
@@ -231,6 +112,7 @@ export default function NewAd() {
               <br />
               <br />
               <input
+                className="text-field"
                 type="text"
                 name="picture"
                 placeholder="Lien de l'image"
@@ -240,6 +122,7 @@ export default function NewAd() {
               <br />
               <br />
               <input
+                className="text-field"
                 type="number"
                 name="price"
                 placeholder="0,00€"
@@ -249,6 +132,7 @@ export default function NewAd() {
               <br />
               <br />
               <input
+                className="text-field"
                 type="text"
                 name="owner"
                 placeholder="Nom du vendeur"
@@ -258,6 +142,7 @@ export default function NewAd() {
               <br />
               <br />
               <input
+                className="text-field"
                 type="text"
                 name="location"
                 placeholder="Localisation"
@@ -267,6 +152,7 @@ export default function NewAd() {
               <br />
               <br />
               <select
+                className="text-field"
                 name="category"
                 value={categoryId + ""}
                 onChange={(e) => setCategoryId(Number(e.target.value))}
@@ -279,7 +165,9 @@ export default function NewAd() {
               </select>
               <br />
               <br />
-              <button type="submit">Poster mon annonce</button>
+              <button type="submit" className="button">
+                Poster mon annonce
+              </button>
             </form>
           </>
         )}
